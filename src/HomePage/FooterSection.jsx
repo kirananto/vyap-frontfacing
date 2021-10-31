754db72c91f76a329916848690745c2d7c4df336
Kiran Anto <kirananto@gmail.com>
2021-10-31 15:38:37 +0530

    fix: improve the address section

--- src/HomePage/FooterSection.jsx
+++ src/HomePage/FooterSection.jsx
@@ -10,11 +10,20 @@ export default function FooterSection() {
                 <div className="flex flex-wrap md:text-left text-center order-first">
 
                     <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                        <h2 className="title-font font-bold text-gray-900 tracking-widest text-sm mb-3">Find us at</h2>
+                        <h2 className="title-font font-bold text-gray-900 tracking-widest text-sm mb-3">Reach out to us at</h2>
                         <nav className="list-none mb-10">
-                            Kerala startup mission(society), District panchayat building, near civil station, PO Vidyanagar, Kasargod
-                            <br />
-                            Pin: 671123
+                            <div className="flex gap-2 mb-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                                </svg>
+                                <a href="mailto:mail@vyap.app"> mail@vyap.app</a>
+                            </div>
+                            <div className="flex gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                                </svg>
+                                <a href="tel:+919995966550">+919995966550</a>
+                            </div>
                         </nav>
                     </div>
                     <div className="lg:w-1/4 md:w-1/2 w-full px-4">
