afcdb3591d3c22caaccb7330f523c9357190868f
Kiran Anto <kirananto@gmail.com>
2021-10-31 15:51:24 +0530

    fix: add typeform url

--- src/HomePage/FooterSection.jsx
+++ src/HomePage/FooterSection.jsx
@@ -61,16 +61,17 @@ export default function FooterSection() {
             </nav> */}
                     </div>
                     <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SUBSCRIBE</h2>
+                        <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">Download the app</h2>
                         <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
-                            <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
+                            Public beta coming soon...
+                            {/* <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
                                 <label for="footer-field" className="leading-7 text-sm text-gray-600">Email</label>
                                 <input type="text" id="footer-field" name="footer-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-blue-200 focus:border-blue-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                             </div>
-                            <button className="mt-2 w-40 mr-2 md:w-auto lg:mt-2 xl:mt-0 flex-shrink-0 text-center text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">Subscribe</button>
+                            <button className="mt-2 w-40 mr-2 md:w-auto lg:mt-2 xl:mt-0 flex-shrink-0 text-center text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">Subscribe</button> */}
                         </div>
-                        <p className="text-gray-400 text-sm mt-2 md:text-left text-center">We will never share your email with external agents.
-                        </p>
+                        {/* <p className="text-gray-400 text-sm mt-2 md:text-left text-center">We will never share your email with external agents.
+                        </p> */}
                     </div>
                 </div>
             </div>
--- src/HomePage/HeroLanding.jsx
+++ src/HomePage/HeroLanding.jsx
@@ -15,7 +15,7 @@ export default function HeroLanding() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                         </svg>
-                        <span className="ml-2">I'm interested </span>
+                        <a className="ml-2" href="https://dmiwhwj8z1s.typeform.com/to/P9AjmCJl" target="_blank" rel="noopener noreferrer">I'm interested </a>
                     </a>
                     {/* <a className="text-indigo-600 hover:underline" href="#">Learn more</a>  */}
                 </div>
