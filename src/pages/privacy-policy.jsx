39db8539d1b53188e8e4e9a2b3bcd923f8d5c82d
Kiran Anto <kirananto@gmail.com>
2021-11-04 14:52:27 +0530

    feat: add privacy-policy page

---
+++ src/pages/privacy-policy.jsx
@@ -0,0 +1,365 @@
+import * as React from "react"
+import Seo from "../seo"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main className="bg-gradient-to-r from-indigo-50 via-blue-50 to-purple-50">
+      <Seo />
+      <div className="w-full max-w-4xl m-auto py-36">
+        <h1
+          className="text-3xl font-bold title-font bg-clip-text text-transparent bg-gradient-to-r from-indigo-600 via-blue-400 to-purple-600"
+          id="h.h20oh9ijyr56"
+        >
+          Privacy Policy | Vyap
+        </h1>
+        <p className="font-semibold text-gray-600 mb-12">
+          Date: <span className="font-mono"> 04 November 2021</span>
+        </p>
+        <p>
+          <span>This privacy policy (&ldquo;</span>
+          <span>Policy</span>
+          <span>
+            &rdquo;) explains our policy regarding the collection, use, disclosure
+            and transfer of your information by Hellogic Technologies LLP and/or its
+            subsidiary(ies) and/or affiliate(s) (collectively referred to as the
+            &ldquo;
+          </span>
+          <span>Company</span>
+          <span>&rdquo; / &ldquo;</span>
+          <span>We</span>
+          <span>&rdquo;), which operates the website </span>
+          <span>
+            <a href="https://vyap.app">https://vyap.app</a>
+          </span>
+          <span>&nbsp;(&quot;</span>
+          <span>Site</span>
+          <span>&quot;), &ldquo;</span>
+          <span>Vyap App</span>
+          <span>&rdquo; (the &ldquo;</span>
+          <span>App</span>
+          <span>
+            &rdquo;) available on Google Play Store and other similar platforms and
+            other services including but not limited to recording sales, purchase,
+            expense &amp; credit/payment, communicating dues/balances via the site
+            and application, any mobile or internet connected device or otherwise
+            (collectively the &quot;
+          </span>
+          <span>Vyap App Service</span>
+          <span>
+            &quot;). This Privacy Policy forms part and parcel of the Terms of Use
+            for the Vyap App Services. Capitalized terms which have been used here
+            but are undefined shall have the same meaning as attributed to them in
+            the Terms of Use.{" "}
+          </span>
+        </p>
+        <p className="mt-8">
+          <span>
+            As we update, improve and expand the Vyap App Service, this Policy may
+            change, so please refer back to it periodically. By accessing the Site,
+            App or otherwise using the Vyap App Services, you consent to the
+            collection, storage, and use of the personal information you provide
+            (including any changes thereto as provided by you) for any of the
+            services that we offer.{" "}
+          </span>
+        </p>
+        <p className="mt-8">
+          <span>
+            The Company respects the privacy of the Users of the Vyap App Services
+            and is committed to reasonably protect it in all respects. The
+            information about the User as collected by the Company is: (a)
+            information supplied by users and (b) information automatically tracked
+            while using a mobile device having Vyap App Services enabled
+            (collectively referred to as &ldquo;
+          </span>
+          <span>Information</span>
+          <span>&rdquo;). </span>
+        </p>
+        <p className="font-bold mt-12 mb-4 text-lg">
+          <span>Information supplied by users</span>
+        </p>
+        <p>
+          <span>
+            To avail certain Services on the Vyap App, Users are required to provide
+            certain personally identifiable information for the registration process
+            which may include: a) their name, b) phone number, c) business entity
+            information and any other such information as required. The Information
+            as supplied by the users enables us to improve the Vyap App Services and
+            provide you the most user-friendly experience. In some cases/provision
+            of certain service(s) or utility(ies), we may require your contact
+            address as well. Further, upon downloading the App, Users are requested
+            to share access to their contact list (as stored on their mobile device
+            on which the App is downloaded), so as to enable the User to share
+            sales, purchase, payment/credit updates with his customers (who might be
+            or might not be Users). This contact list of the User is uploaded to our
+            cloud database, for the purposes of improving and enhancing the User
+            experience of the App. We also do not share this data with any third
+            party.
+          </span>
+        </p>
+        <p className="mt-8 mb-4">
+          <span>
+            We may access a user&#39;s inventory of installed apps and treat this
+            data as personal or sensitive data subject to the Privacy Policy, Secure
+            Transmission, and Prominent Disclosure requirements of User.
+          </span>
+        </p>
+        <p className="mt-8 mb-4">
+          <span>
+            All required Information is service dependent and the Company may use
+            the above said User Information to, maintain, protect, and improve the
+            Vyap App Services, and for developing new services. We may also use your
+            email address and phone number without further consent for non-marketing
+            or administrative purposes (such as notifying you of major changes, for
+            customer service purposes, providing information about updates to Vyap
+            App Services, billing, etc.).{" "}
+          </span>
+        </p>
+        <p className=" mt-8 mb-4">
+          <span>
+            Any personally identifiable Information provided by you will not be
+            considered as sensitive if it is freely available and/or accessible in
+            the public domain. Further, any reviews, comments, messages, blogs
+            posted/uploaded/conveyed/communicated by Users on the public sections of
+            the Site or an Application Store (like the App Store or Play Store)
+            becomes published content and is not considered personally identifiable
+            Information which is subject to this Privacy Policy.{" "}
+          </span>
+        </p>
+        <p className=" mt-8 mb-4 ">
+          <span>
+            In case you choose to decline to submit personally identifiable
+            Information on the Vyap App/Site, we may not be able to provide certain
+            services on the App/Site to you. We will make reasonable efforts to
+            notify you of the same at the appropriate time. In any case, we will not
+            be liable and or responsible for the denial of certain services to you
+            for lack of you providing the necessary personal Information.{" "}
+          </span>
+        </p>
+        <p className="mt-8 mb-4">
+          <span>
+            When you register with the Vyap App Services, we may contact you from
+            time to time about the update of your personal Information to provide
+            the Users such features that we believe may benefit/interest you.{" "}
+          </span>
+        </p>
+        <p className="font-bold mt-8 mb-4 text-lg">
+          <span>Information automatically tracked while using the app or site</span>
+        </p>
+        <p>
+          <span>Sales/purchase/expense/payment</span>
+          <span>
+            &nbsp;Records: We store all your Sales, purchase, expense &amp; payment
+            records in a cloud-based environment using the services of 3
+          </span>
+          <span className="c4 c8">rd</span>
+          <span>&nbsp;party infrastructure service providers. </span>
+        </p>
+        <p className="mt-8 mb-4 ">
+          <span>
+            <strong>Customer Information:</strong> We store the contact information
+            of your customers. We also keep a log of all the sales, purchase,
+            expense &amp; payment related communication to the customers.{" "}
+          </span>
+        </p>
+        <p className="mt-8 mb-4">
+          <span>
+            <strong>Demographic and Related Information: </strong> We may reference
+            other sources of demographic and other information in order to provide
+            you with more targeted communications and promotions. We use Google
+            Analytics, among others, to track the User behaviour on our website.{" "}
+          </span>
+        </p>
+        <p className="mt-8 mb-4">
+          <span>
+            <strong>Log File Information: </strong> Our Servers automatically
+            collect limited Information about your device&#39;s connection to the
+            internet, including your IP address, when you visit our Site or use the
+            App. We automatically receive and log Information from the App and/or
+            your browser including but not limited to IP address, your device or
+            computer&#39;s name, and your operating system, We may also collect log
+            Information from your device, including but not limited to your
+            location, IP address, your device&#39;s name, device&#39;s serial number
+            or unique identification number (e.g. UDID on your iOS device, Android
+            ID or ADID on your Android Device), your device operating system,
+            browser type and version, CPU speed, and connection speed etc.{" "}
+          </span>
+        </p>
+        <p className="mt-8 mb-4">
+          <span>
+            <strong>Cookies: </strong> To improve the responsiveness of the Site for
+            our Users, we may use &quot;cookies&quot;, or similar electronic tools
+            to collect information to assign each visitor a unique, random number as
+            a User Identification (User ID) to understand the User&#39;s individual
+            interests using the identified computer. Our partners may also assign
+            their own cookies to your browser, a process that we do not control.{" "}
+          </span>
+        </p>
+        <p className="font-bold mt-8 mb-4 text-lg">
+          <span>Link to third party sites/ad-servers</span>
+        </p>
+        <p>
+          <span>
+            The Site may include links to other websites. Such websites are governed
+            by their respective privacy policies, which are beyond our control. Once
+            you leave our servers (you can tell where you are by checking the URL in
+            the location bar on your browser), use of any information you provide is
+            governed by the privacy policy of the operator of the site you are
+            visiting. That policy may differ from ours. If you can&#39;t find the
+            privacy policy of any of these sites via a link from the website&#39;s
+            homepage, you should contact the website directly for more information.{" "}
+          </span>
+        </p>
+        <p className="font-bold mt-8 mb-4 text-lg">
+          <span>Information sharing</span>
+        </p>
+        <p>
+          <span>
+            The Company may share the Information (including sensitive personal
+            information) with any third party without obtaining the prior consent of
+            the User in the following limited circumstances:
+          </span>
+        </p>
+        <p className="mt-8 mb-4">
+          <span>
+            When it is requested or required by law or by any court or governmental
+            agency or authority to disclose, for the purpose of verification of
+            identity, or for the prevention, detection, investigation including
+            cyber incidents, or for prosecution and punishment of offences. These
+            disclosures are made in good faith and belief that such disclosure is
+            reasonably necessary for enforcing the Terms of Use or for complying
+            with the applicable laws and regulations. No personally identifiable
+            data of an individual User such as name, phone number, email address,
+            spends data, card details etc. would be shared with any other User
+            and/or third party unless explicitly approved by the concerned
+            individual User himself/herself. However, if this conflicts with
+            conditions mentioned in the immediate point above, this condition would
+            become null and void. The Company may also present information related
+            to credit records, User spends, patterns and User data tracked by the
+            company only in the form of aggregated statistics on data such as
+            overall app usage by date, time, balances, etc. within our app/site or
+            to our partners.{" "}
+          </span>
+        </p>
+        <p className="font-bold mt-8 mb-4 text-lg">
+          <span>Accessing and updating personal information</span>
+        </p>
+        <p>
+          <span>
+            When you use the Site (or any of its sub sites), we make good faith
+            efforts to provide you, as and when requested by you, with access to
+            your personal Information and shall further ensure that any personal
+            Information or sensitive personal data or Information found to be
+            inaccurate or deficient shall be corrected or amended as feasible,
+            subject to any requirement for such personal Information or sensitive
+            personal data or Information to be retained by law or for legitimate
+            business purposes. We ask individual Users to identify themselves and
+            the Information requested to be accessed, corrected or removed before
+            processing such requests, and we may decline to process requests that
+            are unreasonably repetitive or systematic, require disproportionate
+            technical effort, jeopardize the privacy of others, or would be
+            extremely impractical (for instance, requests concerning Information
+            residing on backup tapes), or for which access is not otherwise
+            required. In any case, where we provide Information access and
+            correction, we perform this service free of charge, except if doing so
+            would require a disproportionate effort. Because of the way we maintain
+            certain services, after you delete your Information, residual copies may
+            take a period of time before they are deleted from our active servers
+            and may remain in our backup systems.{" "}
+          </span>
+        </p>
+        <p className="font-bold mt-8 mb-4 text-lg">
+          <span>Information storage and backup</span>
+        </p>
+        <p>
+          <span>
+            From time to time, we take backup of your data on Vyap App on our cloud
+            database. This is done for the purpose of enabling Users to get their
+            data back in case their phone&#39;s data becomes unusable, or phone gets
+            lost, or the User moves to a new phone device. We also use this backup
+            to provide useful insights and Information related to their spends such
+            as weekly spends in a month, insights related to spend behaviour, etc.,
+            and to provide Information related to User spends, patterns and User
+            data in the form of aggregated and anonymized statistics on data such as
+            User spends by category, date, time, bank balances, etc.
+          </span>
+        </p>
+        <p>
+          <span>
+            We shall keep your information in our records till the period of time
+            where we can potentially offer the Vyap App Services to you. After such
+            a period of time where Vyap App Services are no longer relevant to you,
+            we remove all the Information from our records
+          </span>
+          <span className="c4 c17">.</span>
+          <span>&nbsp;</span>
+        </p>
+        <p className="font-bold mt-8 mb-4 text-lg">
+          <span>Information security</span>
+        </p>
+        <p>
+          <span>
+            We take appropriate security measures to protect against unauthorized
+            access to or unauthorized alteration, disclosure or destruction of data.
+            These include internal reviews of our data collection, storage and
+            processing practices and security measures, including appropriate
+            encryption and physical security measures to guard against unauthorized
+            access to systems where we store personal data. All Information gathered
+            on the Vyap App is securely stored within the controlled database.
+            Access to the servers is password-protected and is strictly limited.{" "}
+          </span>
+        </p>
+        <p className="font-bold mt-8 mb-4 text-lg">
+          <span>Updates/Changes</span>
+        </p>
+        <p>
+          <span>
+            We may alter our Privacy Policy from time to time to incorporate
+            necessary changes in technology, applicable law or any other variant. In
+            any case, we reserve the right to change (at any point of time) the
+            terms of this Privacy Policy or the Terms of Use. Any changes we make
+            will be effective immediately on notice, which we may give by posting
+            the new policy on the Site. Your use of the Vyap App Services after such
+            notice will be deemed acceptance of such changes. We may also make
+            reasonable efforts to inform you via electronic mail. In any case, you
+            are advised to review this Privacy Policy periodically on the Site to
+            ensure that you are aware of the latest version.{" "}
+          </span>
+        </p>
+        <p className="font-bold mt-8 mb-4 text-lg">
+          <span>Questions/Grievance Redressal</span>
+        </p>
+        <p>
+          <span>
+            In the event you have any grievance relating to the processing of
+            Information provided by you, you may contact our Grievance Office Jazeem
+            at mail@vyap.app or write to us at the following address:{" "}
+          </span>
+        </p>
+        <div className="text-gray-700 mt-4 border border-gray-400 bg-gray-200 rounded-lg p-6 w-1/2">
+          <p>
+            <span className="font-bold">Hellogic Technologies LLP</span>
+          </p>
+          <p>
+            <span>Kerala Startup Mission</span>
+          </p>
+          <p>
+            <span>District panchayat building</span>
+          </p>
+          <p>
+            <span>BC road, Vidyanagar PO</span>
+          </p>
+          <p>
+            <span>Kasaragod</span>
+          </p>
+          <p>
+            <span></span>
+          </p>
+        </div>
+      </div>
+    </main>
+
+  )
+}
+
+export default IndexPage
