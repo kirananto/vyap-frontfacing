5371c3000e93dbc57f2e155a7cb5e1beb31bc9cb
Kiran Anto <kirananto@gmail.com>
2022-02-06 12:33:44 +0530

    chore(deps): upgrade tailwind & gatsby:

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,7 @@ const IndexPage = () => {
                 </svg>
                 <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
                 <a className="inline-flex items-center">
-                  <img alt="testimonial" src="https://dummyimage.com/106x106" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
+                  <img alt="testimonial" src="https://dummyimage.com/106x106" className="w-12 h-12 rounded-full shrink-0 object-cover object-center" />
                   <span className="flex-grow flex flex-col pl-4">
                     <span className="title-font font-medium text-gray-900">Holden Caulfield</span>
                     <span className="text-gray-500 text-sm">UI DEVELOPER</span>
@@ -75,7 +75,7 @@ const IndexPage = () => {
                 </svg>
                 <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p>
                 <a className="inline-flex items-center">
-                  <img alt="testimonial" src="https://dummyimage.com/107x107" className="w-12 h-12 rounded-full flex-shrink-0 object-cover object-center" />
+                  <img alt="testimonial" src="https://dummyimage.com/107x107" className="w-12 h-12 rounded-full shrink-0 object-cover object-center" />
                   <span className="flex-grow flex flex-col pl-4">
                     <span className="title-font font-medium text-gray-900">Alper Kamu</span>
                     <span className="text-gray-500 text-sm">DESIGNER</span>
@@ -141,7 +141,7 @@ const IndexPage = () => {
                 <label for="footer-field" className="leading-7 text-sm text-gray-600">Placeholder</label>
                 <input type="text" id="footer-field" name="footer-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
               </div>
-              <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+              <button className="lg:mt-2 xl:mt-0 shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
             </div>
             <p className="text-gray-500 text-sm mt-2 md:text-left text-center">Bitters chicharrones fanny pack
               <br className="lg:block hidden" />waistcoat green juice
